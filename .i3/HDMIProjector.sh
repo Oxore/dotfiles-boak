68da73b5ba69080e79016013bc8be53525c5d0f3
Oxore <oxore@protonmail.com>
2018-01-15 23:01:23 +0300

    Add actual i3 config

---
+++ .i3/HDMIProjector.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+xrandr --output HDMI1 --off
+xrandr --output HDMI1 --auto --pos -0x-0
+feh --bg-fill ~/Pictures/.wallpaper.jpg
---
+++ .i3/VGAProjector2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+xrandr --output VGA1 --off
+xrandr --output VGA1 --auto --pos -100x-1080
+feh --bg-fill ~/Pictures/.wallpaper.jpg
